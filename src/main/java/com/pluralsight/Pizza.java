eb3f9688da8f79c687d121018cd8efc7e886c5fd
jesicamatul <evelinmtlj@gmail.com>
2025-11-07 14:25:50 -0500

    added toppings list to pizza class

--- src/main/java/com/pluralsight/Pizza.java
+++ src/main/java/com/pluralsight/Pizza.java
@@ -1,28 +1,47 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pizza extends Product{
+private String size;
+    private Crust crust;
+    private List<Topping> meatToppings = new ArrayList<>();
+    private List<Topping> cheeseToppings = new ArrayList<>();
+    private List<Topping> regularToppings = new ArrayList<>();
+    private List<Topping> sauces = new ArrayList<>();
 
     public Pizza(String size, int quantity, double price, String description) {
         super(size, quantity, price, description);
     }
 
     @Override
-    public double getPrice() {
-       double pizzaSize = switch (size.toLowerCase()) {
-           case "personal" -> 8.50;
-           case "medium" -> 12.00;
-           case "large" -> 16.50;
-           default -> 0;
-       };
+   public double getPrice() {
+        double price = getBasePriceBySize();
 
-       //for premium toppings
-      for(Topping toppings : topping) {
-          if(toppings.isPremium()) {
-              pizzaSize += toppings.getExtraPrice();
-          }
-      }
-
+        for(Topping t : meatToppings) {
+            price += t.getExtraPrice();
+        }
+        for(Topping t: cheeseToppings) {
+            price += t.getExtraPrice();
+        }
+        return price;
     }
+
+
+
+
+    private double getBasePriceBySize(){
+        return switch (size.toLowerCase()) {
+            case "personal" -> 8.50;
+            case "medium" -> 12.00;
+            case "large" -> 16.50;
+            default -> 0.0;
+        };
+
+
+
+
 
 
 }
