0f7a20714b23062b8ad201b9b45361d83c9c4b58
jesicamatul <evelinmtlj@gmail.com>
2025-11-07 14:33:15 -0500

    added variables

--- src/main/java/com/pluralsight/Crust.java
+++ src/main/java/com/pluralsight/Crust.java
@@ -1,5 +1,44 @@
 package com.pluralsight;
 
 public class Crust {
-    //TESTING
+    private String name;
+    private boolean isStuffed;
+    private double extraPrice;
+
+
+    public Crust(String name, boolean isStuffed, double extraPrice) {
+        this.name = name;
+        this.isStuffed = isStuffed;
+        this.extraPrice = extraPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isStuffed() {
+        return isStuffed;
+    }
+
+    public void setStuffed(boolean stuffed) {
+        isStuffed = stuffed;
+    }
+
+    public double getExtraPrice() {
+        return extraPrice;
+    }
+
+    public void setExtraPrice(double extraPrice) {
+        this.extraPrice = extraPrice;
+    }
+
+    @Override
+    public String toString() {
+        return name + (isStuffed ? "(Stuffed Crust)" : "" );
+
+    }
 }
--- src/main/java/com/pluralsight/Sauce.java
+++ src/main/java/com/pluralsight/Sauce.java
@@ -2,6 +2,42 @@ package com.pluralsight;
 
 public class Sauce {
     private String name;
+    private boolean isPremium;
+    private double extraPrice;
 
 
+    public Sauce(String name, boolean isPremium, double extraPrice) {
+        this.name = name;
+        this.isPremium = isPremium;
+        this.extraPrice = extraPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public void setPremium(boolean premium) {
+        isPremium = premium;
+    }
+
+    public double getExtraPrice() {
+        return extraPrice;
+    }
+
+    public void setExtraPrice(double extraPrice) {
+        this.extraPrice = extraPrice;
+    }
+
+    @Override
+    public String toString() {
+        return name + (isPremium ? " (Premium)": "");
+    }
 }
